54e00447cc68b0848d5e924db7a06bbf4a6187f9
mihirDP <patilmi4441@gmail.com>
2025-03-01 14:31:58 +0530

    2- UI Design Completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tailwindcss(), react()],
+  // daisyui: {
+  //   themes: ["emerald", "dark"], // ✅ Correct way to define themes
+  // },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+});
