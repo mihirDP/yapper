59b2dfa457795cb88d4a802916043978dc288928
mihirDP <patilmi4441@gmail.com>
2025-03-05 16:00:36 +0530

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5041; //change the port number to 5041 for the backend server btw!
 
-app.use(express.json()); //to parse req.body for post requests
+app.use(express.json({ limit: "5mb" })); //to parse req.body for post requests
 app.use(express.urlencoded({ extended: true })); //to parse form data(urlEncoded)
 
 app.use(
